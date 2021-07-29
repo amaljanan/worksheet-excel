b7b04accd00fbe32dc870b9fdd1f57e3e609505c
amaljanan.k <amaljanan.k@PIC4034.pitsolutions.com>
2021-07-29 09:40:10 +0530

    Second commmit

--- worksheetExport/src/main/java/com/pits/worksheet/WorksheetMain.java
+++ worksheetExport/src/main/java/com/pits/worksheet/WorksheetMain.java
@@ -9,19 +9,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WorksheetMain {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		long start = System.currentTimeMillis();
+		
+		String exportSheetName = null;
+		String customerSheetName = null;
+		String addressSheetName = null;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Export Sheet Name with extension:");
+		exportSheetName = sc.nextLine();
+		
+		System.out.println("Enter Customer Sheet name with extension:");
+		customerSheetName = sc.nextLine();
+		
+		System.out.println("Enter Address Sheet name with Extension:");
+		addressSheetName = sc.nextLine();
+		
 
 		FileInputStream exportfile = new FileInputStream(
-				new File("C:\\Users\\amaljanan.k\\Downloads\\Excel file\\Export.xlsx"));
+				new File(".\\source\\"+exportSheetName));
 
 		XSSFWorkbook workbookExport = new XSSFWorkbook(exportfile);
 		XSSFSheet exportSheet = workbookExport.getSheet("Export");
@@ -42,23 +60,17 @@ public class WorksheetMain {
 		 */
 
 		FileInputStream customerFile = new FileInputStream(
-				new File("C:\\Users\\amaljanan.k\\Downloads\\Excel file\\20210608_PH_FR_Template_Customer.xlsx"));
+				new File(".\\source\\"+customerSheetName));
 
 		XSSFWorkbook workbookCustomer = new XSSFWorkbook(customerFile);
 		XSSFSheet customerSheet = workbookCustomer.getSheet("Customer");
 
 		FileInputStream addressFile = new FileInputStream(
-				new File("C:\\Users\\amaljanan.k\\Downloads\\Excel file\\20210608_ PH_FR_Template_Address.xlsx"));
+				new File(".\\source\\"+addressSheetName));
 
 		XSSFWorkbook workbookAddress = new XSSFWorkbook(addressFile);
 		XSSFSheet addressSheet = workbookAddress.getSheet("Address");
 
-		/*
-		 * for (int a = 4; a <= addressSheet.getLastRowNum(); a++) {
-		 * System.out.println((int)
-		 * addressSheet.getRow(a).getCell(1).getNumericCellValue()); }
-		 * 
-		 */
 		int customerLastRow = customerSheet.getLastRowNum();
 
 		Cell cell = null;
@@ -68,55 +80,70 @@ public class WorksheetMain {
 
 		for (int i = 4; i <= customerLastRow; i++) {
 			String customerEmail = customerSheet.getRow(i).getCell(3).getStringCellValue();
-			
+
 			uid = customerSheet.getRow(i).getCell(1).toString();
 
-			if (customerEmail != "") 
-			{
+			if (customerEmail != "") {
 				for (int j = 1; j <= row; j++) {
 
 					if (("abc" + customerEmail)
-							.equalsIgnoreCase(exportSheet.getRow(j).getCell(0).getStringCellValue())) 
-					{
+							.equalsIgnoreCase(exportSheet.getRow(j).getCell(0).getStringCellValue())) {
 						customerId = exportSheet.getRow(j).getCell(1).getStringCellValue();
 						for (int k = 4; k <= addressSheet.getLastRowNum(); k++) {
-							if (addressSheet.getRow(k).getCell(1) != null) {
+
+							if (addressSheet.getRow(k).getCell(1).toString() != "") {
 
 								tempUid = addressSheet.getRow(k).getCell(1).toString();
 
 								if (uid.equals(tempUid)) {
 									addressSheet.getRow(k).createCell(1);
 									addressSheet.getRow(k).getCell(1).setCellValue(customerId);
-									//break;
+									// break;
 								}
+
+							} else {
+								addressSheet.removeRow(addressSheet.getRow(i));
+								
+						        
 							}
 						}
 					}
 
 				}
-				
+
 				System.out.println(customerId);
 				cell = customerSheet.getRow(i).createCell(1);
 
-				if (customerId != "") 
-				{
+				if (customerId != "") {
 					try {
 						cell.setCellValue(customerId);
 						System.out.println("Inserted");
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-				} 
+				} else {
+					customerSheet.removeRow(customerSheet.getRow(i));
+				}
 
+			} else {
+				customerSheet.removeRow(customerSheet.getRow(i));
 			}
-			
-			
+
+		}
+
+		for (int i = 4; i <= addressSheet.getLastRowNum(); i++) {
+			if (addressSheet.getRow(i).getCell(1).getCellType() == CellType.NUMERIC) {
+				//addressSheet.removeRow(addressSheet.getRow(i));
+				addressSheet.shiftRows(addressSheet.getRow(i).getRowNum() + 1, addressSheet.getLastRowNum() + 1, -1);
+				i--;
+			}
 		}
 
 		// customerFile.close();
 		try {
 			FileOutputStream outFile = new FileOutputStream(
-					new File("C:\\Users\\amaljanan.k\\Downloads\\Excel file\\20210608_PH_FR_Template_Customer.xlsx"));
+					new File(".\\UpdatedFiles\\newCustomer.xlsx"));
+			// C:\\Users\\amaljanan.k\\Downloads\\Excel file\\newCustomer.xlsx
 			workbookCustomer.write(outFile);
 			outFile.close();
 		} catch (Exception e) {
@@ -126,7 +153,7 @@ public class WorksheetMain {
 
 		try {
 			FileOutputStream outFileAddress = new FileOutputStream(
-					new File("C:\\Users\\amaljanan.k\\Downloads\\Excel file\\20210608_ PH_FR_Template_Address.xlsx"));
+					new File(".\\UpdatedFiles\\newAddress.xlsx"));
 			workbookAddress.write(outFileAddress);
 			outFileAddress.close();
 			System.out.println("Finished");
@@ -137,7 +164,7 @@ public class WorksheetMain {
 
 		long end = System.currentTimeMillis();
 		long elapsedTime = end - start;
-		
-		System.out.println("Total time taken: "+elapsedTime);
+
+		System.out.println("Total time taken: " + elapsedTime);
 	}
 }
