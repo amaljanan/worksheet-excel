f2a1e36ee5d8e731abc6b6781d298c0975398a69
amaljanan.k <amaljanan.k@PIC4034.pitsolutions.com>
2021-08-03 09:38:55 +0530

    copying row with inbuilt function

--- worksheetExport/src/main/java/com/pits/worksheet/WorksheetMain.java
+++ worksheetExport/src/main/java/com/pits/worksheet/WorksheetMain.java
@@ -2,25 +2,24 @@ package com.pits.worksheet;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellCopyPolicy;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
 
 public class WorksheetMain {
 
@@ -36,7 +35,7 @@ public class WorksheetMain {
 
 		Scanner sc = new Scanner(System.in);
 
-		long start = System.currentTimeMillis();
+		
 
 		try {
 			list = importExportCSVFile(sc);
@@ -58,12 +57,16 @@ public class WorksheetMain {
 		XSSFWorkbook workbookDeletedAddress = new XSSFWorkbook();
 		XSSFSheet deletedAddressSheet = workbookDeletedAddress.createSheet("Deleted Address");
 
+		long start = System.currentTimeMillis();
+		
 		Cell cell = null;
 		String customerId = "";
 		String uid = "";
 		String tempUid = "";
 		String customerEmail;
-
+		
+		//XSSFRow deleteRow;
+		
 		for (int i = 4; i <= customerSheet.getLastRowNum(); i++) {
 
 			if (customerSheet.getRow(i).getCell(3).getStringCellValue() != "") {
@@ -158,25 +161,29 @@ public class WorksheetMain {
 
 	private static XSSFSheet deleteAddressRow(XSSFSheet addressSheet, XSSFSheet deletedAddressSheet) {
 		int insertRow = 0;
-		Row deleteRow;
+		XSSFRow deleteRow;
 		// Deleting rows of Address which does not have corresponding entry in Customer
 		for (int i = 4; i <= addressSheet.getLastRowNum(); i++) {
 			if (addressSheet.getRow(i).getCell(1).getCellType() == CellType.NUMERIC) {
 				System.out.println("Removing records which does not have corresponding value in customer");
+				
 				deleteRow = deletedAddressSheet.createRow(insertRow++);
-				for (int j = 0; j <= addressSheet.getRow(4).getLastCellNum(); j++) {
-					Cell mycell;
-					mycell = deleteRow.createCell(j, CellType.STRING);
-					if (addressSheet.getRow(i).getCell(j) != null) {
-						// System.out.println(customerSheet.getRow(i).getCell(j).toString());
-						mycell.setCellValue(addressSheet.getRow(i).getCell(j).toString());
-					}
-					if (addressSheet.getRow(i).getCell(1) == null)
-						deleteRow.createCell(15).setCellValue("Reason for deletion: Customer Id is not present ");
-					else
-						deleteRow.createCell(15).setCellValue(
-								"Reason for deletion: Corresponding Customer is not present in customer sheet ");
+				
+				CellCopyPolicy policy = new CellCopyPolicy();
+			    policy.setCopyCellStyle(false); 
+			    try
+			    {
+			    deleteRow.copyRowFrom(addressSheet.getRow(i), policy);
+			    }
+			    catch (Exception e) {
+			    	System.out.println("Exception:"+addressSheet.getRow(i).getCell(1).toString());
 				}
+				
+				if (addressSheet.getRow(i).getCell(1) == null || addressSheet.getRow(i).getCell(1).getCellType() == CellType.BLANK)
+					deleteRow.createCell(15).setCellValue("Reason for deletion: Customer Id is not present ");
+				else
+					deleteRow.createCell(15).setCellValue("Reason for deletion: Corresponding Customer is not present in customer sheet ");
+			
 				addressSheet.shiftRows(addressSheet.getRow(i).getRowNum() + 1, addressSheet.getLastRowNum() + 1, -1);
 				i--;
 			}
@@ -187,7 +194,7 @@ public class WorksheetMain {
 
 	private static XSSFSheet deleteCustomerRow(XSSFSheet customerSheet, XSSFSheet deletedCustomerSheet) {
 		int insertRow = 0;
-		Row deleteRow;
+		XSSFRow deleteRow;
 		// Deleting rows of Customer which does not have mapping in csv/Email
 		for (int i = 4; i <= customerSheet.getLastRowNum(); i++) {
 			try {
@@ -196,24 +203,18 @@ public class WorksheetMain {
 						|| customerSheet.getRow(i).getCell(1).getCellType() == CellType.BLANK) {
 					System.out.println("Removing records which does not have id mapping/Email");
 					deleteRow = deletedCustomerSheet.createRow(insertRow++);
-					for (int j = 0; j <= customerSheet.getRow(4).getLastCellNum(); j++) {
-						Cell mycell;
-						mycell = deleteRow.createCell(j, CellType.STRING);
-						if (customerSheet.getRow(i).getCell(j) != null) {
-							// System.out.println(customerSheet.getRow(i).getCell(j).toString());
-							mycell.setCellValue(customerSheet.getRow(i).getCell(j).toString());
-						}
-						if (customerSheet.getRow(i).getCell(3) == null
-								|| customerSheet.getRow(i).getCell(3).getCellType() == CellType.BLANK)
-							deleteRow.createCell(11)
-									.setCellValue("Reason for deletion : No Email Id Present for this record");
+					
+				      CellCopyPolicy policy = new CellCopyPolicy();
+				      policy.setCopyCellStyle(false);
+				      
+				      deleteRow.copyRowFrom(customerSheet.getRow(i), policy);
+				      
+				      if (customerSheet.getRow(i).getCell(3) == null || customerSheet.getRow(i).getCell(3).getCellType() == CellType.BLANK)
+							deleteRow.createCell(11).setCellValue("Reason for deletion : No Email Id Present for this record");
 						else
-							deleteRow.createCell(11)
-									.setCellValue("Reason for deletion : No Cutomer Id mapping in Export Sheet");
-					}
-
-					customerSheet.shiftRows(customerSheet.getRow(i).getRowNum() + 1, customerSheet.getLastRowNum() + 1,
-							-1);
+							deleteRow.createCell(11).setCellValue("Reason for deletion : No Cutomer Id mapping in Export Sheet");
+				
+					customerSheet.shiftRows(customerSheet.getRow(i).getRowNum() + 1, customerSheet.getLastRowNum() + 1, -1);
 					i--;
 				}
 			} catch (NullPointerException e) {
